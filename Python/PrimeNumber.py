0998ce21f97c82d4bcd189eee4f351b9b9087377
prakrist <prakristmaharjan0008@gmail.com>
2024-11-20 20:38:13 +0545

    updated code

--- Python/PrimeNumber.py
+++ Python/PrimeNumber.py
@@ -1,3 +1,14 @@
 #This program checks whether the number is prime or composite
+num=int(input("Enter a number: "))
+div=num
+count=0
+for x in range(2,num//2+1):
+    if num % x ==0:
+        count+=1
+if count>0:
+    print("Its Composite number")
+else:
+    print("Its Prime")
 
+    
     
