aa50e04c55b1eb9daffb4a33fe866b67f1b72c06
Prakrist Maharjan <159223080+prakrist21@users.noreply.github.com>
2024-12-09 20:57:36 +0545

    Update Check.py

--- Python/Check.py
+++ Python/Check.py
@@ -1,3 +1,5 @@
+# Please write a program which accepts a string from console and print the characters that have even indexes.
+
 word=input("Enter any string: ")
 for x in range(len(word)):
     if x%2==0:
