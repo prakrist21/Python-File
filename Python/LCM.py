e49c41b4c2ec1355bf08da1d6ceec0310d4970c7
prakrist <prakristmaharjan0008@gmail.com>
2024-12-10 20:38:01 +0545

    new file added

---
+++ Python/LCM.py
@@ -0,0 +1,21 @@
+#This program finds the LCM (Lowest Common Multiple) between given numebers
+num=int(input("Enter the number of numbers you want to enter: "))
+lst=[]
+m=1
+for x in range(num):
+    number=int(input("Enter a number: "))
+    lst.append(number)
+
+for x in lst:
+    m=m*x
+
+for x in range(2,m):
+    lstm=0
+    try:
+        for o in lst:
+            lstm+=x%o
+        if lstm==0:
+            print(x)
+            break
+    except:
+        pass
