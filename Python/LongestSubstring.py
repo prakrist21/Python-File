93379cd5cf0397b215692d305eeda925e4a9cfbd
prakrist <prakristmaharjan0008@gmail.com>
2024-12-13 18:09:49 +0545

    new file

---
+++ Python/LongestSubstring.py
@@ -0,0 +1,18 @@
+# Longest Substring Without Repeating Characters
+
+word=input("Enter a word: ")
+lst=[]
+for x in range(len(word)):
+    new=f'{word[x]}'
+    for y in word[x+1:]:
+        if y in new:
+            break
+        new=new+y
+    lst.append(new)
+# print(lst)
+# print(max([len(x) for x in lst]))
+
+for x in lst:
+    if len(x)==max([len(x) for x in lst]):
+        print("The longest substring without reapating a character is: ",x)
+        print("The length of longest substring without reapating a character is: ",max([len(x) for x in lst]))
