166e3f4767875a0f726e7a1795cfe8ae544c95fc
prakrist <prakristmaharjan0008@gmail.com>
2025-03-08 15:08:49 +0545

    new files

--- Projects/Scraping Daraz Selenium/main.py
+++ Projects/Scraping Daraz Selenium/main.py
@@ -1,3 +1,4 @@
+# Scraping Mobile phones in daraz website. [Only pages 1-3]
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
