c22af75fba94fedd1e5dfec98f7ac21e20bc183f
prakrist <prakristmaharjan0008@gmail.com>
2025-03-19 20:49:18 +0545

    new changes

--- Projects/Scraping Espncricinfo Selenium/BowlersType.py
+++ Projects/Scraping Espncricinfo Selenium/BowlersType.py
@@ -1,5 +1,4 @@
-Spinner=["Varun Chakravarthy","Kuldeep Yadav","Axar Patel","Ravindra Jadeja","Mitchell Santner","Rachin Ravindra","Michael Bracewell","Glenn Phillips","Ish Sodhi","Michael Bracewell","Khushdil Shah","Shadab Khan"
-]
+Spinner=["Varun Chakravarthy","Kuldeep Yadav","Axar Patel","Ravindra Jadeja","Mitchell Santner","Rachin Ravindra","Michael Bracewell","Glenn Phillips","Ish Sodhi","Michael Bracewell","Khushdil Shah","Shadab Khan"]
 
 Pacer=["Kyle Jamieson","Will O'Rourke","Nathan Smith","Mohammed Shami","Hardik Pandya","Jacob Duffy","Zakary Foulkes","Ben Sears","James Neesham","Shaheen Shah Afridi","Mohammad Ali","Haris Rauf","Jahandad Khan"]
 
