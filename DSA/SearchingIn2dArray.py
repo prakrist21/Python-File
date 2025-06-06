fc8db4c1dc0029998c585b247288921384f1887d
prakrist <prakristmaharjan0008@gmail.com>
2025-06-06 11:18:24 +0545

    Searching in 2d Array

--- DSA/SearchingIn2dArray.py
+++ DSA/SearchingIn2dArray.py
@@ -8,7 +8,6 @@ def search(arr,target):
         if(key==target):
             print("Target found at: (",x,",",y,")")
             break
-            exit
         elif(target>key):
             x+=1
 
@@ -17,7 +16,31 @@ def search(arr,target):
     if(key!=target):
         print("The target is not in the array.")
 
+def search2(arr,target):
+    # Method 2: by accessing arr[len(arr)-1][0]
+    x=len(arr)-1
+    y=0
 
+    while(x>=0 and y<len(arr[0])):
+        key=arr[x][y]
+        if(key==target):
+            print("Target found at: (",x,",",y,")")
+            break
 
-search([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],20)
+        elif(key>target):
+            x-=1
+        
+        elif(key<target):
+            y+=1
+            
+    if(key!=target):
+        print("Target not in array")
+
+# Search 1
+search([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],3)
+search([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],25)
 search([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],29)
+# Search 2
+search2([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],3)
+search2([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],25)
+search2([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]],29)
