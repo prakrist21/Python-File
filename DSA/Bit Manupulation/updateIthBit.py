b91ac8270a1e7c1ec63485c0b788ba98275f3eae
prakrist <prakristmaharjan0008@gmail.com>
2025-06-10 11:57:26 +0545

    updating bits

--- DSA/Bit Manupulation/updateIthBit.py
+++ DSA/Bit Manupulation/updateIthBit.py
@@ -1,4 +1,6 @@
 def update(num,i,newbit):
+    # Updates the ith bit into newbit
+    # eg: num=1111(15),i=2,newbit=0 | output: num=1011(11)
     BitMask1=~(1<<i)
     num=num&BitMask1
     BitMask2=(newbit<<i)
