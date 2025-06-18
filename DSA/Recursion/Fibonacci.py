e45cc3c25270edcb4ada6be5fb7ea3ac4ea9f4a5
prakrist <prakristmaharjan0008@gmail.com>
2025-06-18 11:57:06 +0545

    recursion

---
+++ DSA/Recursion/Fibonacci.py
@@ -0,0 +1,17 @@
+def fibo(n):
+    if(n==0) or (n==1):
+        return 1
+
+    return fibo(n-1)+fibo(n-2)
+
+print(fibo(1))
+print(fibo(2))
+print(fibo(3))
+print(fibo(4))
+print(fibo(5))
+print(fibo(6))
+print(fibo(7))
+print(fibo(8))
+print(fibo(9))
+print(fibo(10))
+
